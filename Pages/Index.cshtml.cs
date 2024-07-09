4af6140597d12bfd640fcbcf0459a28c91ed5039
Dapo <Dapo.owolabi@outlook.com>
2024-07-09 19:37:30 -0400

    Data in a Razor Page & Stying a Razor Page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.Website.Services;
+using ContosoCrafts.Website.Models;
 
 namespace ContosoCrafts.Website.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IJsonFileProductService _productService;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IEnumerable<Product> Products { get; private set; }
+
+    public IndexModel(ILogger<IndexModel> logger, IJsonFileProductService productService)
     {
         _logger = logger;
+        _productService = productService;
     }
 
-    public void OnGet()
+    public void OnGet() //HTTP calls
     {
-
+       Products = _productService.GetProducts();
     }
 }
 
